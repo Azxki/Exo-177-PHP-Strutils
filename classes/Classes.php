f7cb1ed749e3eb405b007aaa00f8a851dad6daf9
azxki <cococoursan@gmail.com>
2023-01-27 10:45:30 +0100

    exo

---
+++ classes/Classes.php
@@ -0,0 +1,12 @@
+<?php
+
+class Exercices
+{
+
+    private $str;
+
+    public $bold;
+    public $italic;
+    public $underline;
+    public $capitalize;
+}
--- index.php
+++ index.php
@@ -2,3 +2,11 @@
 
 // TODO n'oubliez pas de créer votre classe dans le dossier classes.
 
+require "./classes/Classes.php";
+
+$exo = new Exercices();
+
+$exo->bold = "<style>body {font-weight: bold;}</style>";
+$exo->capitalize = "<style>body {text-transform: capitalize}</style>";
+$exo->underline = "<style>body {text-decoration: underline}</style>";
+$exo->italic = "<style>body {font-style: italic}</style>";
