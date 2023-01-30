7597b41935802b81aadbc768a646089039e5d685
azxki <cococoursan@gmail.com>
2023-01-30 10:22:08 +0100

    exo

--- classes/Classes.php
+++ classes/Classes.php
@@ -3,10 +3,112 @@
 class Exercices
 {
 
-    private $str;
+    private $str = "<p>Chaine à manipuler</p>";
+    public $bold = "font-weight: bold;";
+    public $italic = "font-style: italic;";
+    public $underline = "text-decoration: underline black;";
+    public $capitalize = "text-transform: capitalize;";
+    public $uglify = "font-weight: bold; font-style: italic; text-decoration: underline black;";
 
-    public $bold;
-    public $italic;
-    public $underline;
-    public $capitalize;
+    public function __construct()
+    {
+        $this->str = "<p style='$this->uglify'>Hello</p>";
+    }
+
+    /**
+     * @return string
+     */
+    public function getStr(): string
+    {
+        return $this->str;
+    }
+
+    /**
+     * @param string $str
+     */
+    public function setStr(string $str): void
+    {
+        $this->str = $str;
+    }
+
+    /**
+     * @return string
+     */
+    public function getBold(): string
+    {
+        return $this->bold;
+    }
+
+    /**
+     * @param string $bold
+     */
+    public function setBold(string $bold): void
+    {
+        $this->bold = $bold;
+    }
+
+    /**
+     * @return string
+     */
+    public function getItalic(): string
+    {
+        return $this->italic;
+    }
+
+    /**
+     * @param string $italic
+     */
+    public function setItalic(string $italic): void
+    {
+        $this->italic = $italic;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUnderline(): string
+    {
+        return $this->underline;
+    }
+
+    /**
+     * @param string $underline
+     */
+    public function setUnderline(string $underline): void
+    {
+        $this->underline = $underline;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCapitalize(): string
+    {
+        return $this->capitalize;
+    }
+
+    /**
+     * @param string $capitalize
+     */
+    public function setCapitalize(string $capitalize): void
+    {
+        $this->capitalize = $capitalize;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUglify(): string
+    {
+        return $this->uglify;
+    }
+
+    /**
+     * @param string $uglify
+     */
+    public function setUglify(string $uglify): void
+    {
+        $this->uglify = $uglify;
+    }
+
 }
--- index.php
+++ index.php
@@ -6,7 +6,4 @@ require "./classes/Classes.php";
 
 $exo = new Exercices();
 
-$exo->bold = "<style>body {font-weight: bold;}</style>";
-$exo->capitalize = "<style>body {text-transform: capitalize}</style>";
-$exo->underline = "<style>body {text-decoration: underline}</style>";
-$exo->italic = "<style>body {font-style: italic}</style>";
+echo $exo->getStr();
